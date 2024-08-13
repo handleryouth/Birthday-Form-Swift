09888e72fccdf6c28e832a568ebe9551357da08e
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 11:18:51 +0700

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Tony Gultom on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
